5e53ed0b22ed21b1433f489f7d387f3ae57a44ee
vinodvr81 <vinod.vukkalam@gmail.com>
2025-07-01 18:29:32 +0530

    jwt authentication changed to component

--- src/main/java/com/sopromadze/blogapi/security/JwtAuthenticationFilter.java
+++ src/main/java/com/sopromadze/blogapi/security/JwtAuthenticationFilter.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,6 +18,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	private static final Logger LOGGER = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
 	@Autowired
